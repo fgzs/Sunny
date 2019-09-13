661b555d78d27f4fb203053ba6b3f54e7b9d930a
Tracy <fgzsmyf@gmail.com>
2019-09-13 15:40:12 +0800

    add Auth  User Controller

--- READMED.md
+++ READMED.md
@@ -1,5 +1,10 @@
 ##Sunny程序
 
-##Date 2019.09.11
+##资料
+[Spring 文档](https://spring.io)
+[Spring Web](https://spring.io/guides)
+[Github 文档](https://developer.github.com)
+[Bootstrap 文档](https://v3.bootcss.com)
+[Github OAuth 文档](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app)
   
 ##记录第一次GitHub
---
+++ src/main/java/com/example/sunny/controller/AuthorizeController.java
@@ -0,0 +1,35 @@
+package com.example.sunny.controller;
+
+import com.example.sunny.dto.AccessTokenDTO;
+import com.example.sunny.dto.GithubUser;
+import com.example.sunny.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id("0d6a2ae304fa62a11ed7");
+        accessTokenDTO.setClient_secret("388915ca29d60bb75be20f9c3cb7c7f714f6f305");
+
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
