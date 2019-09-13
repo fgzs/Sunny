cb6b8785bc94d0872a477d2063d4589e7e8f1cc0
Tracy <fgzsmyf@gmail.com>
2019-09-13 16:31:02 +0800

    extract github config from code to properties

--- src/main/java/com/example/sunny/controller/AuthorizeController.java
+++ src/main/java/com/example/sunny/controller/AuthorizeController.java
@@ -3,10 +3,17 @@ package com.example.sunny.controller;
 import com.example.sunny.dto.AccessTokenDTO;
 import com.example.sunny.dto.GithubUser;
 import com.example.sunny.provider.GithubProvider;
+import com.sun.javaws.IconUtil;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
 
 @Controller
 public class AuthorizeController {
@@ -14,17 +21,28 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+
 
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
 
+
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id("0d6a2ae304fa62a11ed7");
-        accessTokenDTO.setClient_secret("388915ca29d60bb75be20f9c3cb7c7f714f6f305");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
 
 
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
